f0a608ab25ea6a3a87252221a22d5f9b4c32673f
prarshahdv <prarthana.shah@dataverze.ai>
2024-04-05 16:10:50 +0530

    updated requirements.txt

--- api_1.py
+++ api_1.py
@@ -17,26 +17,25 @@ st.title('🦜🔗 Conversation AI Form')
 _FIRST_MESSAGE = "Hello there!, How can I help you today?"
 _DEFAULT_TEMPLATE = """
 You are an interactive conversational chatbot collecting specs for dashboard creation.
-Your goal is to collect user information in a conversational and non-intrusive manner, one piece at a time.
-When asking for details, explain why you need them, and be persuasive yet empathetic.
+Your goal is to collect information in a conversational and non-intrusive manner, one piece at a time from ask_for list.
 Build rapport by transitioning into small talk when appropriate, but aim to gather data smoothly as the conversation progresses.
 If a user hesitates or is unsure, provide reassurance, offer alternatives, and if the user wishes to correct or update their details, be flexible and handle it trustworthily, be polite.
-If no information is needed, thank the user and tell them their dashboard is ready.
+Be polite and nice, continue the ongoing conversation using previous conversation.
 
 Conversation Workflow:
-1. Explain the need for collecting information.
-2. Ask for one piece of information from the 'ask_for' list.
-3. If the user provides the information, confirm it empathetically and move to the next piece when appropriate.
-4. If the user hesitates, offer reassurance, and smoothly transition into small talk or other topics.
-5. When the 'ask_for' list is empty, thank the user and offer further assistance.
-6. Remember not to use greetings or list questions; keep it conversational.
-7. Don't revel information to the user unless they ask for it.
-8. Strictly don't use AI: and bot: kind of prefix in output.
+1. Ask for only one piece of information from the 'ask_for' list, mostly the first one.
+2. If the user provides the information, confirm it empathetically and move to the next piece when appropriate.
+3. When the 'ask_for' list is empty, tell to user to feel free to add or alter any existing information and thank them.
+4. Remember not to use greetings or list questions; keep it conversational.
+5. Don't revel information to the user unless they ask for it.
+6. Strictly don't use AI: and bot: kind of prefix in output, dont greet(dont say Hi or hello, or .., continue the previous conversation).
 
 Previous conversation:
 {history}
+
 Recent user input:
 {input}
+
 Information to ask for (do not ask as a list):
 ### ask_for list: ask_for_list
 
@@ -49,7 +48,7 @@ if "llm" not in st.session_state:
     st.session_state.llm = ChatOpenAI(temperature=0)
 
 if "memories" not in st.session_state:
-    st.session_state.memories = ConversationBufferMemory(k=3)
+    st.session_state.memories = ConversationBufferMemory(k=5)
 
 if "ask_for" not in st.session_state:
     st.session_state.ask_for = list(st.session_state.dashboard_specs.__fields__.keys())
@@ -85,15 +84,14 @@ def add_non_empty_details(new_details: DashboardInfo):
     return
 
 
-def filter_response(text_input):
-    res = st.session_state.chain.run(text_input)
+def filter_response():
+    res = st.session_state.chain.run(str(st.session_state.messages[:-2]))
     add_non_empty_details(res)
     st.write("\t\t\t\tLogs: RES " + str(st.session_state.dashboard_specs))
     ask_for = check_what_is_empty()
     return ask_for
 
-
-def ask_for_info(ask_for=list(st.session_state.dashboard_specs.__fields__.keys()), memories=None, input=""):
+def ask_for_info(ask_for=list(st.session_state.dashboard_specs.__fields__.keys()), input=""):
     global _DEFAULT_TEMPLATE
     PROMPT = PromptTemplate(
         input_variables=["history", "input"],
@@ -103,7 +101,7 @@ def ask_for_info(ask_for=list(st.session_state.dashboard_specs.__fields__.keys()
         llm=st.session_state.llm,
         verbose=True,
         prompt=PROMPT,
-        memory=memories,
+        memory=st.session_state.memories,
     )
     ai_chat = info_gathering_chain.run(input=input)
     return ai_chat
@@ -124,14 +122,11 @@ def main():
         with st.chat_message("user"):
             st.markdown(user_input)
 
-        st.session_state.ask_for = filter_response(user_input)
-        if st.session_state.ask_for:
-            bot_question = ask_for_info(ask_for=st.session_state.ask_for, memories=st.session_state.memories,
-                                        input=str(user_input))
-            st.session_state.messages.append({"role": "assistant", "content": bot_question})
-            write_message("assistant", bot_question)
-        else:
-            st.stop()
+        st.session_state.ask_for = filter_response()
+        bot_question = ask_for_info(ask_for=st.session_state.ask_for,
+                                    input=str(user_input))
+        st.session_state.messages.append({"role": "assistant", "content": bot_question})
+        write_message("assistant", bot_question)
 
 
 print("Starting ...")
--- dashboard_info.py
+++ dashboard_info.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class DashboardInfo(BaseModel):
-    persona: str = Field(
+    persona_or_role_intended_for: str = Field(
         None,
         description="Persona or Role or Title, of the individual we are creating this or designing this dashboard for.",
     )
@@ -15,24 +15,28 @@ class DashboardInfo(BaseModel):
         None,
         description="Target outcome, reason for which we are creating the dashboard or analysis goal.",
     )
-    list_performance_metrics: str = Field(
+    performance_metrics: str = Field(
         None,
-        description="Array or list of Dimensions or Metrics needed for dashboard acceptance criteria, also can be called performance metrics. It can be list/array of multiple acceptance criteria.",
+        description="Metrics needed for dashboard acceptance criteria, it can also contain values and dimensions.",
     )
     additional_information: str = Field(
         "No",
         description="Optional additional information.",
     )
-    requester: str = Field(
+    requester_full_name: str = Field(
         None,
-        description="Who is the requester of this dashboard",
+        description="Name of the requester of this dashboard",
     )
-    urgency: str = Field(
+    urgency_or_priority: str = Field(
         None,
-        enum=["Medium Priority", "Minor", "High Priority"],
+        enum=["Medium Priority", "Low Priority", "High Priority"],
         description="Urgency of the dashboard",
     )
     date: str = Field(
         None,
-        description="By what date do you want the dashboard completed",
+        description="deadline date",
+    )
+    dashboard_view_dimension: str = Field(
+        None,
+        description="Scale to be used for the dashboard, eg. date or days or time or values.",
     )
