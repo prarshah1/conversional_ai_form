c64bcb6350ee9432b2aaea96e5f7fa41a3fdea94
prarshahdv <prarthana.shah@dataverze.ai>
2024-04-05 09:44:43 +0530

    updated dashboard

--- dashboard_info.py
+++ dashboard_info.py
@@ -4,13 +4,39 @@ from pydantic import BaseModel, Field
 class DashboardInfo(BaseModel):
     persona: str = Field(
         None,
-        description="Persona & User Goal",
+        description="Persona & User Goal or Role or Title, of the individual we are creating this dashboard for",
+    )
+    function: str = Field(
+        None,
+        enum=["IT", "HR", "Sales", "Data Science"],
+        description="Which team is the dashboard for",
+    )
+    analysis_goal: str = Field(
+        None,
+        description="Enter the analysis to be done with data to create the dashboard",
+    )
+    business_outcome: str = Field(
+        None,
+        description="Target business outcome",
     )
     metrics: str = Field(
         None,
-        description="Dimensions & Metrics needed for dahsboard",
+        description="Dimensions or Metrics needed for dashboard with respect to the specified requirements",
     )
-    sample_values: str = Field(
+    additional_information: str = Field(
         None,
-        description="sample values of data",
+        description="Optional additional information",
+    )
+    requester: str = Field(
+        None,
+        description="Who is the requester of this dashboard",
+    )
+    urgency: str = Field(
+        None,
+        enum=["Medium Priority", "Minor", "High Priority"],
+        description="Urgency of the dashboard",
+    )
+    due_date: str = Field(
+        None,
+        description="By what date do you want the dashboard completed or Due date",
     )
